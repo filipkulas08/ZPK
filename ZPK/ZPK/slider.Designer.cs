280a12ce2f098e46804ebe20fa3a994b58e2e23d
filipkulas08 <63550459+filipkulas08@users.noreply.github.com>
2020-04-12 19:49:00 +0200

    Add files via upload

---
+++ ZPK/ZPK/slider.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace ZPK
+{
+    partial class slider
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(49, 49);
+            this.trackBar1.Maximum = 255;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(159, 49);
+            this.trackBar2.Maximum = 255;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(104, 45);
+            this.trackBar2.TabIndex = 1;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(269, 49);
+            this.trackBar3.Maximum = 255;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(104, 45);
+            this.trackBar3.TabIndex = 2;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // slider
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "slider";
+            this.Size = new System.Drawing.Size(444, 247);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+    }
+}
---
+++ ZPK/ZPK/slider.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZPK
+{
+    public partial class slider: UserControl
+    {
+        private int ColorR;
+        private int ColorB;
+        private int ColorG;
+        private int SliderValue;
+        private Color Kolor;
+
+       public int Slider1V
+        {
+            get
+            {
+                return trackBar1.Value;
+            }
+            set
+            {
+                trackBar1.Value = SliderValue;
+            }
+        }
+        public int Slider2V
+        {
+            get
+            {
+                return trackBar2.Value;
+            }
+            set
+            {
+                trackBar2.Value = SliderValue;
+            }
+        }
+        public int Slider3V
+        {
+            get
+            {
+                return trackBar3.Value;
+            }
+            set
+            {
+                trackBar3.Value = SliderValue;
+            }
+        }
+        public Color bgColor
+        {
+            get
+            {
+                return Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            }
+            set
+            {
+                Kolor = Color.FromArgb(trackBar1.Value, trackBar2.Value, trackBar3.Value);
+            }
+        }
+
+        public int ColorRed
+        {
+            get
+            {
+                return ColorR;
+            }
+            set
+            {
+                ColorR = value;
+                trackBar1.BackColor = Color.FromArgb(ColorR, 0, 0);
+
+            }
+        }
+        public int ColorGreen
+        {
+            get
+            {
+                return ColorG;
+            }
+            set
+            {
+                ColorG = value;
+                trackBar2.BackColor = Color.FromArgb(0, ColorG, 0);
+            }
+        }
+        public int ColorBlue
+        {
+            get
+            {
+                return ColorB;
+            }
+            set
+            {
+                ColorB = value;
+                trackBar3.BackColor = Color.FromArgb(0, 0, ColorB);
+            }
+        }
+        public slider()
+        {
+            InitializeComponent();
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBar1.BackColor = Color.FromArgb(trackBar1.Value, 0, 0);
+            
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            trackBar2.BackColor = Color.FromArgb(0, trackBar2.Value, 0);
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            trackBar3.BackColor = Color.FromArgb(0, 0, trackBar3.Value);
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
